ca7903a536af50366f84047125f8ab8595579a89
Fadhli Rahim <fadhli@viki.com>
2016-01-22 10:59:58 +0800

    hand stringing json seems clunky. A ruby object to_json helps

--- lib/bitcodin/input/s3_input_config.rb
+++ lib/bitcodin/input/s3_input_config.rb
@@ -4,14 +4,14 @@ module Bitcodin
     attr_accessor :values
 
     def initialize(key:, accessKey: ,secretKey: , bucket:, region:)
-      @values = '{
-        "type": "s3",
-        "accessKey": "' + accessKey + '",
-        "secretKey": "' + secretKey + '",
-        "bucket": "' + bucket + '",
-        "region": "' + region + '",
-        "objectKey": "' + key + '",
-      }'
+      @values = {
+        :type => "s3",
+        :accessKey => "#{accessKey}",
+        :secretKey => "#{secretKey}",
+        :bucket => "#{bucket}",
+        :region => "#{region}",
+        :objectKey => "#{key}",
+      }.to_json
     end
 
   end
