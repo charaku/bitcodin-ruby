37b109dd10a14f42fd0a79be7e6bcea8d7317bdb
Fadhli Rahim <fadhlirahim@gmail.com>
2017-02-17 14:49:49 +0800

    simplify body params builder for encoding_profile endpoint

--- lib/bitcodin/media/encoding_profile.rb
+++ lib/bitcodin/media/encoding_profile.rb
@@ -3,31 +3,23 @@ module Bitcodin
 
     attr_accessor :values
 
-    #def initialize(name, defaultStreamIdVideo, bitrateVideo, profile, preset, height, width, defaultStreamIdAudio, bitrateAudio)
+    # Body parameter building for http://portal.bitcodin.com/api/encoding-profile/create
+    # Usage:
+    #
+    #   profile_config   = Bitcodin::EncodingProfile.new("720-3video-2audio", video_stream_configs, audio_stream_configs)
+    #   profile_config.values
     def initialize(name, videoStreamConfigs, audioStreamConfigs)  
-      @values =  '{'
-      @values += '"name": "' + name + '",'
-      @values += '"videoStreamConfigs": ['
-
-      videoStreamConfigs.each do |videoStreamConfig|
-        @values += videoStreamConfig.values
-        if videoStreamConfig != videoStreamConfigs.last
-          @values += ','
-        end
-      end
-
-      @values += '],'
-      @values += '"audioStreamConfigs": ['
-
-      audioStreamConfigs.each do |audioStreamConfig|
-        @values += audioStreamConfig.values
-        if audioStreamConfig != audioStreamConfigs.last
-          @values += ','
-        end
-      end
-
-      @values += ']}'
+      @values = {
+        "name" => name,
+        "videoStreamConfigs" => map_this(videoStreamConfigs),
+        "audioStreamConfigs" => map_this(audioStreamConfigs)
+      }.to_json 
     end
 
+    private
+
+    def map_this(configs)
+      configs.map{|v| JSON.parse(v.values) }
+    end
   end
 end
